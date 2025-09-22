68979a5a429c7a164e06fe9c144574166f005333
datnopesuy09 <datntph43203@fpt.edu.vn>
2025-09-22 08:08:56 +0700

    fix bug anh don hang cua toi

--- da_be/src/main/java/com/example/da_be/mapper/HoaDonChiTietMapper.java
+++ da_be/src/main/java/com/example/da_be/mapper/HoaDonChiTietMapper.java
@@ -1,13 +1,25 @@
 package com.example.da_be.mapper;
 
 import com.example.da_be.dto.response.HoaDonCTResponse;
+import com.example.da_be.entity.HinhAnh;
 import com.example.da_be.entity.HoaDonCT;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface HoaDonChiTietMapper {
 
+    @Mapping(source = "sanPhamCT.hinhAnh", target = "hinhAnhUrl")
     HoaDonCTResponse toHoaDonChiTietResponse(HoaDonCT hoaDonCT);
+
+    // Hàm hỗ trợ map List<HinhAnh> -> String
+    default String map(List<HinhAnh> hinhAnhs) {
+        if (hinhAnhs == null || hinhAnhs.isEmpty()) {
+            return null;
+        }
+        return hinhAnhs.get(0).getLink();
+    }
 
 }
